a17bcae5f08dddb1e88a701aad3beed0a5289c4c
Lívia Borges <142513001+liviabs@users.noreply.github.com>
2026-05-04 22:31:29 -0300

    Remove Home route

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login    from "./componentes/Login/Login";
 import Register from "./componentes/Login/Register";
-import Home     from "./componentes/Login/Home";
 import BemVindo from "./componentes/Login/BemVindo";
 import Produtos from "./componentes/Produtos/Produtos";
 import Perfil   from "./componentes/Perfil/Perfil";
@@ -22,8 +21,7 @@ function App() {
           <Route path="/"         element={<Login />} />
           <Route path="/login"    element={<Login />} />
           <Route path="/register" element={<Register />} />
-
-          <Route path="/home"     element={<PrivateRoute><Home /></PrivateRoute>} />
+  
           <Route path="/bemvindo" element={<PrivateRoute><BemVindo /></PrivateRoute>} />
           <Route path="/produtos" element={<PrivateRoute><Produtos /></PrivateRoute>} />
           <Route path="/perfil"   element={<PrivateRoute><Perfil /></PrivateRoute>} />
